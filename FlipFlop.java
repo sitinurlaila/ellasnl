31f8a3427d784fc10e3ac9e2f2e2b45e830df3ae
sitinurlaila <sitinurlaila@outlook.com>
2014-04-21 01:01:30 +0700

    initial project
    error flipflop and palindrom

---
+++ FlipFlop.java
@@ -0,0 +1,166 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testsitinurlaila;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author ^_^
+ */
+public class FlipFlop extends javax.swing.JFrame {
+String a;
+int b;
+int i;
+    
+    public FlipFlop() {
+        initComponents();
+    }  
+    
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setText("Masukan Angka =");
+
+        jTextField1.setBackground(new java.awt.Color(255, 153, 51));
+        jTextField1.addActionListener(new java.awt.event.ActionListener() {
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField1ActionPerformed(evt);
+            }
+        });
+
+        jTextArea1.setBackground(new java.awt.Color(51, 204, 255));
+        jTextArea1.setColumns(20);
+        jTextArea1.setFont(new java.awt.Font("Verdana", 0, 11)); // NOI18N
+        jTextArea1.setRows(5);
+        jTextArea1.setBorder(new javax.swing.border.MatteBorder(null));
+        jScrollPane1.setViewportView(jTextArea1);
+
+        jButton1.setBackground(new java.awt.Color(204, 255, 204));
+        jButton1.setText("Cek");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jButton2.setBackground(new java.awt.Color(255, 102, 102));
+        jButton2.setText("Clear All");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        jLabel2.setText("Hasil :");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 102, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 149, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(28, 28, 28)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 43, Short.MAX_VALUE))
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 397, Short.MAX_VALUE)
+                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 160, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(23, 23, 23)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton1))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton2)
+                .addGap(15, 15, 15)
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 183, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+
+        pack();
+    }
+
+    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {
+    }
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {
+        proses();
+    }
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {
+        clear();
+    }
+
+
+    public void proses(){
+        if (jTextField1.getText().trim().equals(""))
+        {
+            JOptionPane.showMessageDialog(null, "Field angka wajib diisi", "alert", JOptionPane.ERROR_MESSAGE);
+        }
+        else
+        {
+        String deret = "";
+        a = jTextField1.getText();
+        b = Integer.parseInt(a);
+        b = b;
+        for (){
+            
+
+            jTextArea1.setText(deret);
+            }
+        }       
+    }
+    public void clear(){
+        jTextField1.setText("");
+        jTextArea1.setText("");
+        JOptionPane.showMessageDialog(null, "Data has empty", "Infromation", JOptionPane.INFORMATION_MESSAGE);
+    }
+    /**
+    * @param args the command line arguments
+    */
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new FlipFlop().setVisible(true);
+            }
+        });
+    }
+    
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JTextField jTextField1;
+
+}
---
+++ Palindrom.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testsitinurlaila;
+
+import java.io.*;
+
+/**
+ *
+ * @author ^_^
+ */
+public class Palindrom{
+
+public static void main(String [] args) throws IOException
+
+{
+
+StringBuffer sb;
+
+BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
+System.out.print("Masukkan sebuah kata: ");
+
+String kata = input.readLine();
+
+sb = new StringBuffer(kata);
+
+
+
+{
+
+System.out.println("\n\n\nPalindrom");
+
+}
+
+else
+
+{
+
+System.out.println("\n\n\nBukan Palindrom");
+
+}
+
+}
+
+}
